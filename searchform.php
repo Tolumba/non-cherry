8b98d8ccd040564abc13f24973f95024be744680
Tolumba <Tolumba@users.noreply.github.com>
2016-08-15 12:29:38 +0300

    Add files via upload

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form class="search" method="get" id="search" action="<?php echo esc_url( home_url() ); ?>">
+
+	<input type="text" class="searching" value="<?php the_search_query(); ?>" name="s" id="s" />
+	<a class="btn btn-default" onclick="document.getElementById('search').submit()"><?php _e( 'search', 'non-cherry' ); ?></a>
+
+</form>
