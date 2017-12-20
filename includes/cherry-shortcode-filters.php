05a9a68e4daea1d84bae10674c20fe9430e2eaa1
Tolumba <tolumbas@gmail.com>
2017-12-20 13:41:24 +0200

    ADD: Adapter class for Cherry_Shortcodes_Template_Callbacks

--- includes/cherry-shortcode-filters.php
+++ includes/cherry-shortcode-filters.php
@@ -103,7 +103,7 @@ function non_cherry_banner_shortcode( $result, $atts=array(), $shortcode='' ){
 	return $result;
 }
 
-if(class_exists('Cherry_Shortcodes_Handler')){
+if( class_exists('Cherry_Shortcodes_Handler') ){
 	Cherry_Shortcodes_Handler::$macros_pattern = '/%%([a-zA-Z_]+)(\s)?([^%]+)?%%/';
 }
 
@@ -115,10 +115,21 @@ function non_cherry_shortcode_postdata( $data, $post_id=null, $atts=array() ){
 	if( isset( $atts['lightbox_image'] ) && ( 'yes' === $atts['lightbox_image'] ) )
 		wp_enqueue_script( 'magnific-popup' );
 
+	try{
+		$instance = $data['title'][0];
+	}catch( Exeption $e ){
+		
+	}
+
+	if( is_a( $instance, 'Cherry_Shortcodes_Template_Callbacks' ) ){
+
+		$adapter = new Cherry_Shortcodes_Template_Callbacks_Adapter( $instance );
+		$data['button'] = array( $adapter, 'button' );
+	}
+
 	$data['imageurl'] = 'non_cherry_imageurl_callback';
 	$data['custommeta'] = 'non_cherry_custom_meta_callback';
 	$data['custommetavalue'] = 'non_cherry_custom_meta_value_callback';
-	$data['imageurl'] = 'non_cherry_imageurl_callback';
 	$data['title_text'] = 'non_cherry_title_text_callback';
 	$data['types_field'] = 'non_cherry_render_types_field';
 
@@ -140,7 +151,6 @@ function non_cherry_grid_shortcode_data_callbacks( $data, $atts=array() ){
 	$data['imageurl'] = 'non_cherry_imageurl_callback';
 	$data['custommeta'] = 'non_cherry_custom_meta_callback';
 	$data['custommetavalue'] = 'non_cherry_custom_meta_value_callback';
-	$data['imageurl'] = 'non_cherry_imageurl_callback';
 	$data['types_field'] = 'non_cherry_render_types_field';
 
 	return $data;
@@ -162,9 +172,11 @@ function non_cherry_title_text_callback( $params=null ){
 // Callback function for %%IMAGEURL%% macros
 function non_cherry_imageurl_callback( $params='post-thumbnail' ){
 
-	global $post;
+	$size_pattern = '#^=[\'\"]([a-z_\-]+)[\'\"][,;]?#';
+	preg_match( $size_pattern, $params, $maches );
+	$params = count($maches)? $maches[1]: $params;
 
-	$post_type = get_post_type( $post->ID );
+	$post_type = get_post_type( get_the_ID() );
 
 	if ( ! post_type_supports( $post_type, 'thumbnail' ) ) {
 		return;
@@ -189,8 +201,11 @@ function non_cherry_imageurl_callback( $params='post-thumbnail' ){
 // Callback function for %%IMAGE%% macros
 function non_cherry_grid_image_callback( $params='post-thumbnail' ){
 
-	global $post;
-	$post_type = get_post_type( $post->ID );
+	$size_pattern = '#^=[\'\"]([a-z_\-]+)[\'\"][,;]?#';
+	preg_match( $size_pattern, $params, $maches );
+	$params = count($maches)? $maches[1]: $params;
+
+	$post_type = get_post_type( get_the_ID() );
 
 	if ( ! post_type_supports( $post_type, 'thumbnail' ) ) {
 		return;
@@ -200,7 +215,7 @@ function non_cherry_grid_image_callback( $params='post-thumbnail' ){
 		return;
 	}
 
-	$grid_meta = get_post_meta( $post->ID, '_cherry_grid', true );
+	$grid_meta = get_post_meta( get_the_ID(), '_cherry_grid', true );
 	
 	if ( isset( $grid_meta['show_thumb'] ) && 'no' == $grid_meta['show_thumb'] ) {
 		return;
@@ -213,12 +228,12 @@ function non_cherry_grid_image_callback( $params='post-thumbnail' ){
 	$img_src = wp_get_attachment_image_src( $thumb, $size );
 
 	if(is_array($img_src)){
-		$image = '<img src="'.$img_src[0].'" alt="'.get_the_title($post).'">';
+		$image = '<img src="'.$img_src[0].'" alt="'.get_the_title(get_the_ID()).'">';
 	}else{
 		$image = '';
 	}
 
-	return sprintf( $format, $image, get_the_permalink($post) );
+	return sprintf( $format, $image, get_the_permalink(get_the_ID()) );
 }
 
 function non_cherry_custom_meta_callback( $meta_tag=null ){
@@ -226,10 +241,12 @@ function non_cherry_custom_meta_callback( $meta_tag=null ){
 	if( !$meta_tag )
 		return;
 
+	$meta_tag_pattern = '#^=[\'\"]([a-z_\-, ;]+)[\'\"][,;]?#';
+	preg_match( $meta_tag_pattern, $params, $maches );
+	$meta_tag = count($maches)? $maches[1]: '';
+
 	$meta_tag_arr = preg_split( "#[, ;]+#", $meta_tag );
 	$format = apply_filters( 'custom-meta-format', '<div class="meta-box %1$s">%2$s</div>' );
-
-	//var_dump(get_post_custom(get_the_ID()));
 
 	if( count( $meta_tag_arr ) > 1 ){
 
@@ -272,6 +289,10 @@ function non_cherry_custom_meta_value_callback( $meta_tag=null ){
 	if( !$meta_tag )
 		return;
 
+	$meta_tag_pattern = '#^=[\'\"]([a-z_\-, ;]+)[\'\"][,;]?#';
+	preg_match( $meta_tag_pattern, $params, $maches );
+	$meta_tag = count($maches)? $maches[1]: '';
+
 	$meta_tag_arr = preg_split( "#[, ;]+#", $meta_tag );
 
 	if( count( $meta_tag_arr ) > 1 ){
@@ -310,7 +331,7 @@ function non_cherry_render_types_field( $params=null ){
 	if( !$params )
 		return;
 
-	$meta_tag_pattern = '#^=[\'\"]([a-z_-]+)[\'\"][,;]?#';
+	$meta_tag_pattern = '#^=[\'\"]([a-z_\-]+)[\'\"][,;]?#';
 	preg_match( $meta_tag_pattern, $params, $maches );
 	$meta_tag = count($maches)?$maches[1]:'';
 
@@ -392,4 +413,172 @@ function non_cherry_portfolio_postdata( $_postdata, $post_id = null ){
 	return $_postdata;
 };
 
+class Cherry_Shortcodes_Template_Callbacks_Adapter{
+
+	public $atts = array();
+
+	public $content = null;
+
+	public function __construct( $instance ){
+		$this->atts = $instance->atts;
+		$this->content = $instance->content;
+	}
+
+	public function button( $params=null ) {
+
+		$classes_pattern = '#^=[\'\"]([\w\s]+)[\'\"][,;]?#';
+		preg_match( $classes_pattern, $params, $maches );
+		$classes = count($maches)? $maches[1]: '';
+
+		$shortcode   = Cherry_Shortcodes_Handler::get_shortcode_name();
+		$button_text = sanitize_text_field( $this->atts['button_text'] );
+
+		if ( empty( $button_text ) ) {
+			return;
+		}
+
+		$button = sprintf(
+			'<a href="%1$s" class="%2$s">%3$s</a>',
+			esc_url( get_permalink( get_the_ID() ) ),
+			esc_attr( $classes ),
+			$button_text
+		);
+
+		/**
+		 * Filter a button.
+		 *
+		 * @since 1.0.0
+		 * @param string $date      Button with HTML-markup.
+		 * @param array  $atts      Shortcode attributes.
+		 * @param string $shortcode Shortcode name.
+		 */
+		return apply_filters( 'cherry_shortcodes_button_template_callbacks', $button, $this->atts, $shortcode );
+	}
+
+	public function date( $params=null ) {
+
+		$format_pattern = '#^=[\'\"]([a-z_\-]+)[\'\"][,;]?#';
+		preg_match( $format_pattern, $params, $maches );
+		$format = count($maches)? $maches[1]: '';
+
+		$shortcode = Cherry_Shortcodes_Handler::get_shortcode_name();
+
+		if ( empty( $format ) ) {
+			$format = get_option( 'date_format' );
+		}
+
+		$date = sprintf(
+			'<time datetime="%1$s">%2$s</time>',
+			esc_attr( get_the_date( 'c' ) ),
+			esc_attr( get_the_date( $format, get_the_ID() ) )
+		);
+
+		/**
+		 * Filter a date.
+		 *
+		 * @since 1.0.0
+		 * @param string $date      Date with HTML-markup.
+		 * @param array  $atts      Shortcode attributes.
+		 * @param string $shortcode Shortcode name.
+		 */
+		return apply_filters( 'cherry_shortcodes_date_template_callbacks', $date, $this->atts, $shortcode );
+	}
+
+	public function image( $params=null ) {
+
+		$size_pattern = '#^=[\'\"]([a-z_\-]+)[\'\"][,;]?#';
+		preg_match( $size_pattern, $params, $maches );
+		$size = count($maches)? $maches[1]: '';
+
+		$shortcode = Cherry_Shortcodes_Handler::get_shortcode_name();
+
+		if ( 'banner' == $shortcode ) {
+			return Cherry_Shortcodes_Tools::get_image_url( $this->atts['image'] );
+		}
+
+		if ( ! post_type_supports( get_post_type( get_the_ID() ), 'thumbnail' ) ) {
+			return;
+		}
+
+		if ( ! has_post_thumbnail( get_the_ID() ) ) {
+			return;
+		}
+
+		$url = get_permalink( get_the_ID() );
+		$image_classes   = array();
+		$image_classes[] = 'post-thumbnail_link';
+
+		switch ( $shortcode ) {
+			case 'posts':
+
+				if ( empty( $size ) ) {
+					$size = sanitize_key( $this->atts['image_size'] );
+				}
+
+				if ( isset( $this->atts['lightbox_image'] ) && ( 'yes' === $this->atts['lightbox_image'] ) ) {
+					$image_classes[] = 'cherry-popup-img';
+					$image_classes[] = 'popup-img';
+
+					$thumbnail_id = get_post_thumbnail_id( get_the_ID() );
+					$url          = wp_get_attachment_url( $thumbnail_id );
+
+					if ( ! $url ) {
+						$url = get_permalink( get_the_ID() );
+					}
+
+					wp_enqueue_script( 'magnific-popup' );
+				}
+
+				$thumbnail = get_the_post_thumbnail( get_the_ID(), $size );
+				break;
+
+			case 'swiper_carousel':
+				$post_id     = get_the_ID();
+				$crop_image  = ( bool ) ( 'yes' === $this->atts['crop_image'] ) ? true : false;
+				$crop_width  = intval( $this->atts['crop_width'] );
+				$crop_height = intval( $this->atts['crop_height'] );
+
+				if ( $crop_image ) {
+					$img_url   = wp_get_attachment_url( get_post_thumbnail_id(), 'full' );
+					$thumbnail = Cherry_Shortcodes_Tools::get_crop_image( $img_url, get_post_thumbnail_id(), $crop_width, $crop_height );
+				} else {
+					$thumbnail = get_the_post_thumbnail( get_the_ID(), 'large' );
+				}
+				break;
+
+			default:
+
+				if ( empty( $this->atts['image_size'] ) ) {
+					$thumbnail = get_the_post_thumbnail( get_the_ID(), 'large' );
+				} else {
+					$thumbnail = get_the_post_thumbnail( get_the_ID(), sanitize_key( $this->atts['image_size'] ) );
+				}
+				break;
+		}
+
+		$wrap = ( 'no' === $this->atts['linked_image'] ) ? '%3$s' : '<a href="%1$s" title="%2$s" class="%4$s">%3$s</a>';
+
+		$image_classes = apply_filters( 'cherry_shortcodes_image_classes_template_callbacks', $image_classes, $shortcode );
+		$image_classes = array_unique( $image_classes );
+		$image_classes = array_map( 'sanitize_html_class', $image_classes );
+
+		$image = sprintf(
+			$wrap,
+			esc_url( $url ),
+			esc_attr( the_title_attribute( array( 'before' => '', 'after' => '', 'echo' => false, 'post' => get_the_ID() ) ) ),
+			$thumbnail,
+			join( ' ', $image_classes )
+		);
+
+		/**
+		 * Filter a image.
+		 *
+		 * @since 1.0.0
+		 * @param string $image     Image tag.
+		 * @param array  $atts      Shortcode attributes.
+		 * @param string $shortcode Shortcode name.
+		 */
+		return apply_filters( 'cherry_shortcodes_image_template_callbacks', $image, $this->atts, $shortcode );
+	}
+}
 ?>
