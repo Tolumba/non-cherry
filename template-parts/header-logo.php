14e5296426deaa20ef0af4e14c8cda021a1921fb
Tolumba <Tolumba@users.noreply.github.com>
2019-03-29 16:40:44 +0200

    Update header-logo.php

--- template-parts/header-logo.php
+++ template-parts/header-logo.php
@@ -1,7 +1,7 @@
 <div class="site-logo">
 	<h1 class="logo">
 		<?php if(of_get_option('logo_type') == 'text_logo'){?>
-			<a href="<?php echo esc_url( home_url() ); ?>/" title="<?php bloginfo('description'); ?>"><span class="main"><?php bloginfo('name'); ?></span><br/><span class="secondary"><?php bloginfo('description'); ?></span></a>
+			<a href="<?php echo esc_url( home_url("/") ); ?>" title="<?php bloginfo('description'); ?>"><span class="main"><?php bloginfo('name'); ?></span><br/><span class="secondary"><?php bloginfo('description'); ?></span></a>
 		<?php } else {
 				$logo_src = wp_get_attachment_url( (int) of_get_option( 'logo_url' ) );
 				$logo_src = $logo_src ?: CHILD_URL."/images/logo.png";
