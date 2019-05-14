65dcc46ffb8a3ee71370c2277aac34298512b738
Tolumbas <tolumbas@gmail.com>
2019-05-14 17:34:50 +0300

    UPD: Added Shop Sidebar Switch

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,12 @@
+<?php 
+$sidebar = 'main-sidebar';
+
+if ( function_exists( 'is_shop_page' ) && is_shop_page() ) {
+	$sidebar = 'sidebar-shop';
+} ?>
 <?php if(of_get_option('blog_sidebar_pos')!='none'): ?>
 	<aside id="sidebar" class="<?php loop_sidebar_classes(); ?>">
-		<?php if ( ! dynamic_sidebar( 'main-sidebar' )) : ?>
+		<?php if ( ! dynamic_sidebar( $sidebar )) : ?>
 			<!-- No widgets so far :( -->
 		<?php endif; ?>
 	</aside><!--sidebar-->
